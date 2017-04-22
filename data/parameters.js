205633f857f438481395237d9c51d7cd85f01ac9
Miryam Houali <miryam.houali@free.fr>
2017-04-22 16:28:27 +0200

    first attempt at displaying speech bubbles when clicking on objects

--- data/parameters.js
+++ data/parameters.js
@@ -2,8 +2,13 @@
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 const GAME_BACKGROUND_COLOR = '#FFFFFF'
-const TEXT_COLOR = "#2f2320"
+const TEXT_COLOR = "#000"
+
 // Assets
 const ASSETS = [
   {"name":"bg", "src":"assets/ld38_bg_001.png", "type":"image"},
+  {"name":"object1", "src":"assets/ld38_object_001.png", "type":"image"},
+  {"name":"object2", "src":"assets/ld38_object_002.png", "type":"image"},
+  {"name":"bubble", "src":"assets/ld38_bubble_001.png", "type":"image"},
+  {"name":"character", "src":"assets/ld38_character_001.png", "type":"image"}
 ];

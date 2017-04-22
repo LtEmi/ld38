79f6f85c34f7b24bcd563e2f6f39e16109113023
Miryam Houali <miryam.houali@free.fr>
2017-04-22 12:54:11 +0200

    init assets

--- data/parameters.js
+++ data/parameters.js
@@ -3,7 +3,7 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 const GAME_BACKGROUND_COLOR = '#FFFFFF'
 const TEXT_COLOR = "#2f2320"
-//// Assets
-//const ASSETS = [
-//  {"name":"square+raw_1x2", "src":"assets/ld36_square+raw_1x2_001.png", "type":"image"},
-//];
+// Assets
+const ASSETS = [
+  {"name":"bg", "src":"assets/ld38_bg_001.png", "type":"image"},
+];

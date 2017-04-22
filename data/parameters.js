edb7af5bb6b537cfe2a254069db0cda69ec182d8
Miryam Houali <miryam.houali@free.fr>
2017-04-22 12:37:30 +0200

    setting assets

--- data/parameters.js
+++ data/parameters.js
@@ -3,14 +3,6 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 const GAME_BACKGROUND_COLOR = '#FFFFFF'
 const TEXT_COLOR = "#2f2320"
-//// Grid constantes
-//const GRID_WIDTH = 480;
-//const GRID_X = 53;
-//const GRID_Y = 63;
-//const GRID_SQUARES_X = 18;
-//const GRID_SQUARES_Y = 12;
-//// Others
-//const SQUARE_WIDTH = 40;
 //// Assets
 //const ASSETS = [
 //  {"name":"square+raw_1x2", "src":"assets/ld36_square+raw_1x2_001.png", "type":"image"},
